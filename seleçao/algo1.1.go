5222bbf18f10e7e15ebdc34b0717b880e5402aac
kavay1 <lucas.souzab@sempreceub.com>
2023-03-21 20:19:22 -0300

    selecao

--- seleçao/algo1.1.go
+++ seleçao/algo1.1.go
@@ -1,5 +1,19 @@
 package main
 
+import "fmt"
+
 func main() {
+
+	var num1, num2 int
+
+	fmt.Println("Digite dois numeros:")
+	fmt.Scan(&num1, &num2)
+
+	if num1 > num2 {
+		fmt.Println("O maior numero é", num1)
+
+	} else {
+		fmt.Println("O maior numero é:", num2)
+	}
 
 }
