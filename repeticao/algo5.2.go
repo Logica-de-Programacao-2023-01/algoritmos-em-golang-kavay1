3b8cfa2adc2595ff93f05cfc263f7a68fe34f735
kavay1 <lucas.souzab@sempreceub.com>
2023-03-28 12:40:01 -0300

    repeticao

---
+++ repeticao/algo5.2.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	for num := 10; num <= 1; num-- {
+		if num == 1 {
+			break
+			
+		}
+		fmt.Println(num)
+	}
+}
---
+++ repeticao/algo6.2.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var num int
+
+	fmt.Println("Digite um número:")
+	fmt.Scan(&num)
+
+	fmt.Println("A tabuada de 1 a 10 do seu número é:")
+	for multi := 1; multi <= 10; multi++{
+		
+		fmt.Println(multi*num)
+
+
+	}
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	}
